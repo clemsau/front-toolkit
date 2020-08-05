ab22cf8fbf474c5fe6b457078bfeed23dc331f5e
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-08-05 22:16:03 +0200

    Update style

--- components/ToolCard.js
+++ components/ToolCard.js
@@ -1,14 +1,20 @@
 export default function ToolCard({name, description, url, tags}) {
     return (
         <a href={url} target="_blank">
-            <div className="tool-card s-padded-container">
-                <h2>{name}</h2>
-                <p>{description}</p>
-                <ul>
-                {tags.map(tag => (
-                    <li className="btn btn-light-blue">{tag}</li>
-                ))}
-                </ul>
+            <div className="tool-card s-padded-container row">
+                <div className="card-title-content">
+                    <h2>{name}</h2>
+                </div>
+                <div className="card-decription-content">
+                    <p>{description}</p>
+                </div>
+                <div className="card-tags-content">
+                    <ul>
+                    {tags.map(tag => (
+                        <li className="btn btn-light-blue">{tag}</li>
+                    ))}
+                    </ul>
+                </div>                
             </div>
         </a>
     )
