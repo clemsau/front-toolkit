f4142443034b20653cfdee2ee472a0c9bb52f658
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-08-05 20:18:13 +0200

    Change image background to CSS gradient

--- components/ToolCard.js
+++ components/ToolCard.js
@@ -1,13 +1,14 @@
 export default function ToolCard({name, description, url, tags}) {
     return (
-        <div>
-            <a href={url} target="_blank"><h1>{name}</h1></a>
+        <div className="tool-card">
+            <a href={url} target="_blank"><h1>{name}</h1>
             <p>{description}</p>
             <ul>
             {tags.map(tag => (
                 <li>{tag}</li>
             ))}
             </ul>
+            </a>
         </div>
     )
 }
